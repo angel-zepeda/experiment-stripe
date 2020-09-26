22f533f1288d6c9b52dca78d15b3618e4d6d74b5
angel-zepeda <angel.zepeda0508@gmail.com>
2020-09-25 19:20:51 -0500

    chore(stripe): Remove api keys

--- utils/stripe.ts
+++ utils/stripe.ts
@@ -1,14 +1,11 @@
 import Stripe from 'stripe';
 import { loadStripe } from '@stripe/stripe-js';
 
-export const stripe = new Stripe(
-  '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',
-  {
-    apiVersion: '2020-08-27'
-  }
-);
+export const stripe = new Stripe(process.env.STRIPE_SECRET_KEY, {
+  apiVersion: '2020-08-27'
+});
 
-export const stripePromise = loadStripe('pk_live_BEmo6Ye1B1y7xLOZF9dmAEMo001HAgmz8j');
+export const stripePromise = loadStripe(process.env.STRIPE_PUBLIC_KEY);
 
 export const createSubscription = async (priceId: string) => {
   const stripe = await stripePromise;
